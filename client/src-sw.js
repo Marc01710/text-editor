9061a268e88832580302ccf14680462d5be2469e
WeenoCoding <hernandez17marc@gmail.com>
2023-12-15 11:59:47 -0800

    last commit

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,11 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+// const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+// const { CacheFirst } = require('workbox-strategies');
+// const { registerRoute } = require('workbox-routing');
+// const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+// const { ExpirationPlugin } = require('workbox-expiration');
+// const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies'); 
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -19,12 +25,19 @@ const pageCache = new CacheFirst({
   ],
 });
 
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+
